312ddd1adb6f7440c405e9f613da548219dd58d4
zhaosg <zhaosg89@163.com>
2017-11-15 23:05:14 +0800

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        List<String> list=new ArrayList<>();
+        while(true){
+            list.add("6C0CBF5029AED0AF396C0CBF5029AED0AF395AC4B864C6B0956C0CBF5029AED0AF395AC4B864C6B0956C0CBF5029AED0AF395AC4B864C6B0956C0CBF5029AED0AF395AC4B864C6B0956C0CBF5029AED0AF395AC4B864C6B0956C0CBF5029AED0AF395AC4B864C6B0956C0CBF5029AED0AF395AC4B864C6B0955AC4B864C6B095");
+            Thread.sleep(1);
+        }
+    }
+}
